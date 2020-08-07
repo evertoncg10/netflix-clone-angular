3c0f476aeddad189869882251ffd50bd016656d0
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2020-08-07 14:33:57 -0300

    Separação em componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
+
 const routes: Routes = [];
 
 @NgModule({
