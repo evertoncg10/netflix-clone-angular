b80ae1943de762b488c3eb6c6b2d96b743723dad
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2020-08-10 13:07:03 -0300

    Propriedades dos Componentes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  listTitlesRecentes = [{
+    trailer: 'assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
+    cover: 'assets/images/guardios-da-galaxia.jpg',
+    name: 'Guardiões da Galaxia 1',
+    relevance: 98,
+    age: 12,
+    parts: 2,
+    categories: ['Épico', 'Filme de fantasia', 'Viagem no espaço']
+  }, {
+    trailer: 'assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
+    cover: 'assets/images/guardios-da-galaxia.jpg',
+    name: 'Guardiões da Galaxia 2',
+    relevance: 78,
+    age: 16,
+    parts: 2,
+    categories: ['Épico', 'Filme de fantasia', 'Viagem no espaço']
+  }, {
+    trailer: 'assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
+    cover: 'assets/images/guardios-da-galaxia.jpg',
+    name: 'Guardiões da Galaxia 3',
+    relevance: 85,
+    age: 18,
+    parts: 2,
+    categories: ['Épico', 'Filme de fantasia', 'Viagem no espaço']
+  }];
 }
